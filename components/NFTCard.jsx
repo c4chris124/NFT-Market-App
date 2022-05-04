bc02023a7557f21c5330f3d7aa515f85f41ab49a
Christian Castellanos <c4chris124@gmail.com>
2022-05-04 13:57:40 -0600

    Applying prittier to code

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -2,7 +2,7 @@ import { View, Image, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
 import { CircleButton, RectButton } from "./Button";
-import { SubInfo, EthPrice, NFTTitle  } from "./SubInfo";
+import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
@@ -17,26 +17,26 @@ const NFTCard = ({ data }) => {
       }}
     >
       <View style={{ width: "100%", height: 250 }}>
-        <Image 
-        source={data.image} 
-        resizeMode="cover" 
-        style={{
-          width: "100%",
-          height: "100%",
-          borderTopLeftRadius: SIZES.font,
-          borderTopRightRadius: SIZES.font,
-        }} 
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
         />
-        <CircleButton imgUrl={assets.heart} right={10} top={10}/>
+        <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
-      <SubInfo/>
-      <View style={{width: '100%', padding: SIZES.font}}>
-        <NFTTitle 
-        title={data.name}
-        subTitle={data.creator}
-        titleSize={SIZES.large}
-        subTitleSize={SIZES.small}
-         />
+      <SubInfo />
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
       </View>
     </View>
   );
