a26cead74cc8ac55d2d68fc06fd10387e42a642a
Christian Castellanos <c4chris124@gmail.com>
2022-05-04 13:56:11 -0600

    Importing nft title component, passing props and styles

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -30,6 +30,14 @@ const NFTCard = ({ data }) => {
         <CircleButton imgUrl={assets.heart} right={10} top={10}/>
       </View>
       <SubInfo/>
+      <View style={{width: '100%', padding: SIZES.font}}>
+        <NFTTitle 
+        title={data.name}
+        subTitle={data.creator}
+        titleSize={SIZES.large}
+        subTitleSize={SIZES.small}
+         />
+      </View>
     </View>
   );
 };
